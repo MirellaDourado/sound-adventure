eacfa5e73811a60997726f60d4f40c821031869d
MirellaDourado <mirellaalvesdourado@gmail.com>
2023-08-04 12:38:55 -0300

    feat: adding enemies data to another file

--- js/data/collisions.js
+++ js/data/collisions.js
@@ -8,19 +8,6 @@ const collisionsLevel1 = [
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 ]
 
-const enemyLevel1 = [{
-  minimumLimit: 470,
-  maximumLimit: 630,
-  position: {
-    x: 470,
-    y: 360,
-  },
-  velocity: {
-    x: 2,
-    y: 0,
-  }
-}];
-
 const collisionsLevel2 = [
   292, 292, 292, 292, 292, 292, 292, 0, 0, 0, 0, 0, 0, 0, 0, 0, 292, 0, 0, 0, 0,
   0, 292, 0, 0, 0, 0, 0, 0, 0, 0, 0, 292, 0, 0, 0, 0, 0, 292, 0, 0, 0, 0, 0, 0,
@@ -30,20 +17,6 @@ const collisionsLevel2 = [
   0, 0, 292, 0, 0, 292, 0, 0, 0, 0, 0, 0, 0, 0, 292, 292, 292, 292, 292, 0, 0,
   292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 292, 0, 0, 0, 0,
 ]
-
-const enemyLevel2 = [{
-  minimumLimit: 420,
-  maximumLimit: 600,
-  position: {
-    x: 500,
-    y: 500,
-  },
-  velocity: {
-    x: 0,
-    y: 0,
-  }
-}];
-
 
 const collisionsLevel3 = [
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 250, 250, 250, 250, 250,
---
+++ js/data/enemyData.js
@@ -0,0 +1,29 @@
+const enemyLevel1 = [{
+  minimumLimit: 470,
+  maximumLimit: 630,
+  position: {
+    x: 470,
+    y: 360,
+  },
+  velocity: {
+    x: 2,
+    y: 0,
+  }
+}];
+
+const enemyLevel2 = [{
+  minimumLimit: 420,
+  maximumLimit: 600,
+  position: {
+    x: 500,
+    y: 500,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  }
+}];
+
+const enemyLevel3 = [{
+  
+}]
